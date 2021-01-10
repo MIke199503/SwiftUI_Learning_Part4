636ba04e465da306fb610c059be800631386259e
Mike199503 <zth19950326@yahoo.com>
2021-01-10 12:11:31 +0800

    Initial Commit

---
+++ Shared/DesignCodeCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeCourseApp.swift
+//  Shared
+//
+//  Created by 朱桃禾 on 2021/1/10.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
