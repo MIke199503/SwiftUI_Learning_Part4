571078c0432cfade1f5bc984c530c6ea2dec0562
Mike199503 <zth19950326@yahoo.com>
2021-01-16 19:57:45 +0800

    2021-01-16提交

--- README.md
+++ README.md
@@ -34,6 +34,9 @@
 **Date : 2021 - 01 - 15 :**    
     Finished the 12th without problem,  In this lesson, we use lazyVGrid to automatically arrange views, V for vertical, or we can use lazyHgrid, parameters that should be rows, not columns
 
+**Date : 2021 - 01 - 16 :**    
+    Finished the 13th without problem,  In this lesson, we use Zindex to determine which view is at the top level. we created a CloseButton ,there's all 
+
 ## 3⃣️ Catelog：
 01. [<u> Build a SwiftUI app for iOS 14 </u>](https://www.acfun.cn/v/ac17870993_1)
 02. [<u> SwiftUI Visual Editor  </u>](https://www.acfun.cn/v/ac17870993_2)
@@ -47,4 +50,5 @@
 10. [<u> Matchede Cards and Static Data </u>](https://www.acfun.cn/v/ac17870993_10)
 11. [<u> Selection State and Disable </u>](https://www.acfun.cn/v/ac17870993_11)
 12. [<u> Lazy grid layout  </u>](https://www.acfun.cn/v/ac17870993_12)
-13. 
+13. [<u> Matched Container and zindex  </u>](https://www.acfun.cn/v/ac17870993_13)
+
--- Shared/CoursesView.swift
+++ Shared/CoursesView.swift
@@ -25,32 +25,82 @@ struct CoursesView: View {
                     //                ) {
                     
                     ForEach(courses) { item in
-                        CourseItem(course: item)
-                            .matchedGeometryEffect(id: item.id, in: namespace,isSource: !show)
-                            //这里的matchedgeometryEffect 我觉得就跟PPT中的平滑移动很类似，id的话，就是匹配，从谁变到谁，然后in的话，我的理解，就是一个空间用来变换的。然后issource就是谁是根试图，也就是从谁那里变得。
-                            .frame(height: 200)
-                            //将OntapGesture放在这里是为了对应每个card
-                            .onTapGesture {
-                                withAnimation(.spring()) {
-                                    self.show.toggle()
-                                    self.selectedItem = item
-                                    self.isDisabled = true
+                        VStack {
+                            CourseItem(course: item)
+                                .matchedGeometryEffect(id: item.id, in: namespace,isSource: !show)
+                                //这里的matchedgeometryEffect 我觉得就跟PPT中的平滑移动很类似，id的话，就是匹配，从谁变到谁，然后in的话，我的理解，就是一个空间用来变换的。然后issource就是谁是根试图，也就是从谁那里变得。
+                                .frame(height: 200)
+                                //将OntapGesture放在这里是为了对应每个card
+                                .onTapGesture {
+                                    withAnimation(.spring(response: 0.4, dampingFraction: 0.8, blendDuration: 0)) {
+                                        self.show.toggle()
+                                        self.selectedItem = item
+                                        self.isDisabled = true
+                                    }
                                 }
-                            }
-                            .disabled(self.isDisabled)
+                                .disabled(self.isDisabled)
+                        }
+                        .matchedGeometryEffect(id: "container\(item.id)", in: namespace,isSource: !show)
                     }
                 }
                 .padding(10)
                 .frame(maxWidth:.infinity)
             }
-            
+            .zIndex(1)
             
             if selectedItem != nil   {
-                ScrollView {
-                    CourseItem(course: selectedItem!)
-                        .matchedGeometryEffect(id: selectedItem!.id, in: namespace)
-                        .frame(height:300)
-                        //放在这里，是为了放大之后的额缩回去。
+                ZStack(alignment:.topTrailing){
+                    //这里使用toptrailing 是为了让关闭按钮出现在右上角
+                    VStack {
+                        ScrollView {
+                            CourseItem(course: selectedItem!)
+                                .matchedGeometryEffect(id: selectedItem!.id, in: namespace)
+                                .frame(height:300)
+                                //放在这里，是为了放大之后的额缩回去。
+                                .onTapGesture {
+                                    withAnimation(.spring()) {
+                                        self.show.toggle()
+                                        self.selectedItem = nil
+                                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3 ){
+                                            self.isDisabled = false
+                                        }
+                                    }
+                                }
+                            VStack{
+                                ForEach( 0 ..< 20 ){ item in
+                                    CourseRow()
+                                }
+                            }
+                            .padding()
+                            
+                        }
+                    }
+                    //                        .zIndex(1)//防止遮挡
+                    //这里的transition，我的理解是当你的view和animation链接，然后按照特定的方式出现。
+                    //使用透明度来过渡，并且在过渡的时候，添加一个Spring效果。并且有延迟。
+                    //.asymmetric 就是分开来处理。因为如果同时处理的话，推出的时候，延迟效果没必要，大可不必，兄弟。
+
+                    .background(Color("Background 1"))
+                    .clipShape(RoundedRectangle(cornerRadius: 22,style: .continuous))
+                    .matchedGeometryEffect(id: "container\(selectedItem!.id)", in: namespace,isSource: !show)
+                    .transition(
+                        .asymmetric(
+                            insertion: AnyTransition
+                                .opacity
+                                .animation(
+                                    Animation.spring()
+                                        .delay(0.3)
+                                ),
+                            removal: AnyTransition
+                                .opacity
+                                .animation(.spring()))
+                        
+                    )
+                    .edgesIgnoringSafeArea(.all)
+                    
+                    
+                    
+                    CloseButton()
                         .onTapGesture {
                             withAnimation(.spring()) {
                                 self.show.toggle()
@@ -60,33 +110,9 @@ struct CoursesView: View {
                                 }
                             }
                         }
-                    VStack{
-                        ForEach( 0 ..< 20 ){ item in
-                            CourseRow()
-                        }
-                    }
-                    .padding()
-                    
+
                 }
-                .background(Color("Background 1"))
-                .transition(
-                    .asymmetric(
-                        insertion: AnyTransition
-                            .opacity
-                            .animation(
-                                Animation.spring()
-                                    .delay(0.3)
-                            ),
-                        removal: AnyTransition
-                            .opacity
-                            .animation(.spring()))
-                    
-                )
-                .edgesIgnoringSafeArea(.all)
-                //                        .zIndex(1)//防止遮挡
-                //这里的transition，我的理解是当你的view和animation链接，然后按照特定的方式出现。
-                //使用透明度来过渡，并且在过渡的时候，添加一个Spring效果。并且有延迟。
-                //.asymmetric 就是分开来处理。因为如果同时处理的话，推出的时候，延迟效果没必要，大可不必，兄弟。
+                .zIndex(2)
             }
             
             
